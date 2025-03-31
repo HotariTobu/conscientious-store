adc5646815814518bfd911744bc915cd87deb727
HotariTobu <hotari24tools@gmail.com>
2025-03-31 11:42:15 +0900

    upd: serve.sh for everyone to use easily

--- serve.sh
+++ serve.sh
@@ -1,4 +1,25 @@
+#!/bin/bash
+# Start serving the Next.js application with the latest code from the repository
+
+# Change to the directory of the script
+cd $(dirname $(readlink -f $0))
+
+# Remove the old build artifacts
+rm -rf ./node_modules
+rm -rf ./.next
+
+# Check if Node.js is installed
+node -v
+if [ $? -ne 0 ]; then
+    echo "Node.js is not installed. Please install Node.js to run this script from https://nodejs.org."
+    exit
+fi
+
+# Apply the latest code
 git pull
 npm install
+npm run prisma:migrate
+
+# Build and start the application
 npm run build
 npm run start
